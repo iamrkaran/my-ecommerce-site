0572b4b5237e7741c83cf8f00ed17a62e40c9c7a
iamClutchX <iamr.karan@gmail.com>
2023-08-14 12:11:47 +0530

    logo link

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -13,9 +13,10 @@ const Header: React.FC = () => {
       <div className="container mx-auto px-4 py-2 flex justify-between items-center">
         <div className="logo">
           {/* Add your logo here */}
-          <h1>E-commerce</h1>
+          <Link to="/"><h1>E-commerce</h1>
+          </Link>
         </div>
-        <nav> 
+        <nav>
           <ul className="flex space-x-4">
             <li>
               <Link to="/">Home</Link>
@@ -67,23 +68,10 @@ const Header: React.FC = () => {
           </Link>
           {user ? (
             <Link to="/profile">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-8 w-8 text-gray-800"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10 12a2 2 0 100-4 2 2 0 000 4zm0 2a4 4 0 100-8 4 4 0 000 8z"
-                  clipRule="evenodd"
-                />
-                <path
-                  fillRule="evenodd"
-                  d="M10 20a6 6 0 100-12 6 6 0 000 12zm0 2a8 8 0 100-16 8 8 0 000 16z"
-                  clipRule="evenodd"
-                />
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
               </svg>
+
             </Link>
           ) : (
             <Link to="/login">
